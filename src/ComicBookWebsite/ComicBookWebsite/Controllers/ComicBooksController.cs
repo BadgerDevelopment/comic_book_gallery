41b317280820cb4c63ff062d3bc55081d33dea0b
joey Turner <badgerdevelops@gmail.com>
2019-09-18 08:19:13 -0500

    updated detail action method to use action result types

--- src/ComicBookWebsite/ComicBookWebsite/Controllers/ComicBooksController.cs
+++ src/ComicBookWebsite/ComicBookWebsite/Controllers/ComicBooksController.cs
@@ -8,9 +8,15 @@ namespace ComicBookWebsite.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
-        {
-            return "Hello front he comic book controller!";
+        public ActionResult Detail()
+        {   
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return Redirect("/");
+                return new RedirectResult("/");
+            }
+            return Content("Hello front he comic book controller!");
+             
         }
     }
 }
