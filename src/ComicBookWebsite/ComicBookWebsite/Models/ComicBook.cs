4c043e68e68af0a0f29b86e4a3997928dcd206bb
joey Turner <badgerdevelops@gmail.com>
2019-09-25 06:10:33 -0500

    Updated Comic Book Model

--- src/ComicBookWebsite/ComicBookWebsite/Models/ComicBook.cs
+++ src/ComicBookWebsite/ComicBookWebsite/Models/ComicBook.cs
@@ -13,5 +13,25 @@ namespace ComicBookWebsite.Models
         public string DescriptionHtml { get; set; } 
         public Artist[] Arists { get; set; } 
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+                get
+                {
+                    return SeriesTitle + " #" + IssueNumber;
+                }
+        }
+
+            // series-title-issuenumber.jpg
+            public string CoverImageFileName
+            {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                .ToLower() + "-" + IssueNumber + " .jpg";
+
+
+                }
+            }
     }
 }
